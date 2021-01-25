a3bfe8427ce20a469aadea0ba5bf5ac0a193cb02
heekang2271 <gmlrkd3922@gmail.com>
2021-01-25 16:35:35 +0900

    Search Container 작성

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "../../api";
 
 export default class extends React.Component {
     state = {
@@ -9,6 +10,45 @@ export default class extends React.Component {
         error: null,
         loading: false
     };
+
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        if (searchTerm !== "") {
+            this.searchByTerm();
+        }
+    }
+
+    searchByTerm = async () => {
+        const { searchTerm } = this.state;
+        this.setState({
+            loading: true
+        })
+        try {
+            const {
+                data: {
+                    results: movieResults
+                }
+            } = await moviesApi.search(searchTerm);
+            const {
+                data: {
+                    results: tvResults
+                }
+            } = await tvApi.search(searchTerm);
+
+            this.setState({
+                movieResults,
+                tvResults
+            })
+        } catch {
+            this.setState({
+                error: "Can't find results"
+            })
+        } finally {
+            this.setState({
+                loading: false
+            })
+        }
+    }
 
     render() {
         const { movieResult, tvResult, searchTerm, error, loading } = this.state;
