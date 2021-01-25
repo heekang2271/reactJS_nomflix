5dbbbbeec23b92af6d88551c09a8284c7e6e1aab
heekang2271 <gmlrkd3922@gmail.com>
2021-01-25 11:39:06 +0900

    styled header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Router from './Router.js';
+import GlobalStyles from './GlobalStyles.js';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Router />
+        <GlobalStyles />
       </div>
     );
   }
